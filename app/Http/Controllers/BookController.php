e9024fbe34d8e33260f9c9af7533ccfc62690a25
phanvanhuong <phanvanhuong.htu@gmail.com>
2017-07-24 14:11:49 +0700

    BookController modified

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -60,14 +60,7 @@ class BookController extends Controller
 		$topic = Topic::pluck('name','id');
 		return view('backend.createbook')->with('author',$author)->with('publish',$publish)->with('topic',$topic);
 	}
- 	  {
-    		$author = Author::pluck('name','id');
-    		$publish = PublishCompany::pluck('name','id');
-    		$topic = Topic::pluck('name','id');
-    		return view('backend.createbook')->with('author',$author)->with('publish',$publish)->with('topic',$topic);
-	  }
 
-	public function postCreateBook(Request $request)
   	public function postCreateBook(Request $request)
     {
         $input = Input::all();
@@ -87,31 +80,13 @@ class BookController extends Controller
 		$book = Book::create($input);
 		return redirect('/listbooks');
 	}
-    		$publish = PublishCompany::pluck('name','id');
-    		$topic = Topic::pluck('name','id');
-    		$book = Book::create($input);
-    		return redirect('/listbooks');
-	   }
-
+    	
 	public function listBook()
 	{
 		$books = Book::all();
 		return view('backend.listadmin.listbooks')->with('books',$books);
 	}
-	   public function listBook()
-	   {
-    		$books = Book::all();
-    		return view('backend.listadmin.listbooks')->with('books',$books);
-    	}
 
-	public function editBook($id)
-	{
-		$book = Book::find($id);
-		$author = Author::pluck('name','id');
-		$publish = PublishCompany::pluck('name','id');
-		$topic = Topic::pluck('name','id');
-		return view('backend.editadmin.editbook')->with('book', $book)->with('author', $author)->with('publish', $publish)-> with('topic', $topic);
-	}
     	public function editBook($id)
     	{
     		$book = Book::find($id);
@@ -125,29 +100,8 @@ class BookController extends Controller
 	{
 		$book = Book::find($id);
 		$input = Input::all();
-        if ($request->hasFile('image')) {
-    	public function putEditBook(Request $request,$id)
-    	{
-    		$book = Book::find($id);
-    		$input = Input::all();
-            if ($request->hasFile('image')) {
-
-            $file_name = time() . '-' .$request->file('image')->getClientOriginalName();
-            $input['image'] = $file_name;
-            $request->file('image')->move('upload', $file_name);
-            //delete img 
-			$a = $book['image'];
-			$b = ('upload/'.$a);
-			File::delete($b);
-			// end delete
-			$a = $file_name;
-        }
-        $author = Author::pluck('name','id');
-		$publish = PublishCompany::pluck('name','id');
-		$topic = Topic::pluck('name','id');
-		$book->update($input);
-		return redirect('/listbooks');
-	}
+        if ($request->hasFile('image')) 
+            {
                 $file_name = time() . '-' .$request->file('image')->getClientOriginalName();
                 $input['image'] = $file_name;
                 $request->file('image')->move('upload', $file_name);
@@ -158,6 +112,7 @@ class BookController extends Controller
     			// end delete
     			$a = $file_name;
             }
+            
             $author = Author::pluck('name','id');
     		$publish = PublishCompany::pluck('name','id');
     		$topic = Topic::pluck('name','id');
